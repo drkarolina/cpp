cbcb7c4128d1e9bbfe4cbcb2f00f9e84d5ce4d76
Karolina Drahanchuk <karolina.dr02@gmail.com>
2022-11-21 17:28:28 +0200

    Final fixes

--- src/conteiners/RandomNameGenerator.java
+++ src/conteiners/RandomNameGenerator.java
@@ -5,21 +5,21 @@ import java.util.Arrays;
 
 public class RandomNameGenerator {
     private static final ArrayList<String> names = new ArrayList<>(Arrays.asList(
-        "Neive Ruiz",
-        "Colton Pace",
-        "Jim Hensley",
-        "Hakim Choi",
-        "Farhan Melton",
-        "Bianka Beach",
-        "Nichola Kearns",
-        "Katelyn Estrada",
-        "Alivia Knapp",
-        "Danyl Corona",
-        "Juno Aldred",
-        "Dave Dejesus",
-        "Tonya Albert",
-        "Emma-Louise Keller",
-        "Hina Heath"));
+        "Neive", "Ruiz",
+        "Colton", "Pace",
+        "Jim", "Hensley",
+        "Hakim", "Choi",
+        "Farhan", "Melton",
+        "Bianka", "Beach",
+        "Nichola", "Kearns",
+        "Katelyn", "Estrada",
+        "Alivia", "Knapp",
+        "Danyl", "Corona",
+        "Juno", "Aldred",
+        "Dave", "Dejesus",
+        "Tonya", "Albert",
+        "Emma-Louise", "Keller",
+        "Hina", "Heath"));
 
     public static String GetRandomName(){
         int index = (int)(Math.random() * names.size());
