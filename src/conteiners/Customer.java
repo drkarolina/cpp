1536294bee6382b0122884548d0e63e4c9c574fa
Karolina Drahanchuk <karolina.dr02@gmail.com>
2022-11-21 17:09:07 +0200

    Add customer name and id autogeneration

--- src/conteiners/Customer.java
+++ src/conteiners/Customer.java
@@ -1,14 +1,18 @@
 package conteiners;
 
 public class Customer {
+    static int IDCounter = 0;
     private int line, id;
     private Order order;
     private String name;
-    public Customer(int line, int id, Order order, String name) {
+    public Customer(int line, Order order) {
         this.line = line;
-        this.id = id;
+        this.id = IDCounter;
         this.order = order;
-        this.name = name;
+        this.name = RandomNameGenerator.GetRandomName();
+        
+        IDCounter += 1;
+        Log.AddCustomer(this);
         Log.AddOrder(order);
     }
     public int GetLine() {
